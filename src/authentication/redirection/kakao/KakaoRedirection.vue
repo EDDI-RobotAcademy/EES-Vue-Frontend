ffb77237e5cab3dd2f8529c6709c18f3c94c1a99
Yoonseoparkk <patpark97@gmail.com>
2024-06-16 16:44:09 +0900

    feat: [박윤서] Access Token 카카오에 요청 완료 [EESV-6]

--- src/authentication/redirection/kakao/KakaoRedirection.vue
+++ src/authentication/redirection/kakao/KakaoRedirection.vue
@@ -9,10 +9,13 @@ const accountModule = 'accountModule'
 export default {
     methods: {
         // ...mapActions(authenticationModule, ['requestAccessTokenToDjangoRedirection', 'requestUserInfoToDjango']),
+        ...mapActions(authenticationModule, ['requestAccessTokenToDjangoRedirection', 'requestUserInfoToDjango']),
         // ...mapActions(accountModule, ['requestEmailDuplicationCheckToDjango']),
         async setRedirectData () {
             const code = this.$route.query.code
             console.log('code:', code)
+            await this.requestAccessTokenToDjangoRedirection({ code })
+            // const userInfo = this.requestUserInfoToDjango()
         }
     },
     async created () {
