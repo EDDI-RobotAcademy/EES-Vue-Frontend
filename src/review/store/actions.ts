b8a8da2ab153abe32934c9b9a6b5c1e7e2ed79bb
Yoonseoparkk <patpark97@gmail.com>
2024-06-16 15:06:03 +0900

    feat: review 서버 요청 부분 임시 주석처리

--- src/review/store/actions.ts
+++ src/review/store/actions.ts
@@ -5,22 +5,22 @@ import axiosInst from "@/utility/axiosInstance"
 import { REQUEST_REVIEW_LIST_TO_DJANGO } from "./mutation-types"
 
 export type ReviewActions = {
-    requestReviewListToDjango(context: ActionContext<ReviewState, any>): Promise<void>
+    // requestReviewListToDjango(context: ActionContext<ReviewState, any>): Promise<void>
 }
 
 const actions: ReviewActions = {
-    async requestReviewListToDjango(context: ActionContext<ReviewState, any>): Promise<void> {
-        try {
-            const res: AxiosResponse<any, any> =
-                await axiosInst.djangoAxiosInst.get('/review/list/')
+    // async requestReviewListToDjango(context: ActionContext<ReviewState, any>): Promise<void> {
+    //     try {
+    //         const res: AxiosResponse<any, any> =
+    //             await axiosInst.djangoAxiosInst.get('/review/list')
 
-            const data: Review[] = res.data
-            context.commit(REQUEST_REVIEW_LIST_TO_DJANGO, data)
-        } catch (error) {
-            console.error('requestReviewListToDjango(): ' + error)
-            throw error
-        }
-    }
+    //         const data: Review[] = res.data
+    //         context.commit(REQUEST_REVIEW_LIST_TO_DJANGO, data)
+    //     } catch (error) {
+    //         console.error('requestReviewListToDjango(): ' + error)
+    //         throw error
+    //     }
+    // }
 }
 
 export default actions
