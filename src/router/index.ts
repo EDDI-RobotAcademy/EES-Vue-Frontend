cafd0d383b31618640b94aecd37e7f1af19e2b52
Yoonseoparkk <patpark97@gmail.com>
2024-06-16 16:37:32 +0900

    feat: [박윤서] authentication code 받기 완료 [EESV-6]

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,14 +2,16 @@ import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import ReviewRoutes from '@/review/router/ReviewRoutes'
 import AccountRoutes from '@/account/router/AccountRoutes'
+import AuthenticationRoutes from '@/authentication/router/AuthenticationRoutes'
 
 const routes: Array<RouteRecordRaw> = [
   ...AccountRoutes,
-  ...ReviewRoutes
+  ...ReviewRoutes,
+  ...AuthenticationRoutes,
 ]
 
 const router = createRouter({
-    history: createWebHistory(process.env.BASE_URL),
+  history: createWebHistory(process.env.BASE_URL),
   routes
 })
 export default router
