d848702dc5eb3cf6b6ee345250019c2059cca3ff
Yoonseoparkk <patpark97@gmail.com>
2024-06-16 17:00:44 +0900

    feat: [박윤서] Access Token 카카오에 요청 완료 [EESV-6]

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,16 +2,14 @@ import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import ReviewRoutes from '@/review/router/ReviewRoutes'
 import AccountRoutes from '@/account/router/AccountRoutes'
-import AuthenticationRoutes from '@/authentication/router/AuthenticationRoutes'
 
 const routes: Array<RouteRecordRaw> = [
   ...AccountRoutes,
-  ...ReviewRoutes,
-  ...AuthenticationRoutes,
+  ...ReviewRoutes
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+    history: createWebHistory(process.env.BASE_URL),
   routes
 })
 export default router
