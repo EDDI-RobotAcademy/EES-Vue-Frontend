5b8b24472f1d475263648df5f02c818d2a49bbbb
moneyandjelly <rmsdldy12@gmail.com>
2024-06-22 13:05:34 +0900

    add: [이근] 사용자 리뷰 작성 페이지 추가 [EESV-25]

---
+++ src/review/pages/register/ReviewRegisterPage.vue
@@ -0,0 +1,25 @@
+<template>
+    <v-container>
+        <v-row>
+            <v-col cols="12">
+                <v-text-field v-model="title" label="제목"/>
+            </v-col>
+        </v-row>
+        <v-row>
+            <v-col cols="12">
+                <v-text-field v-model="writer" label="작성자"/>
+            </v-col>
+        </v-row>
+        <v-row>
+            <v-col cols="12">
+                <v-text-field v-model="content" label="내용" auto-grow/>
+            </v-col>
+        </v-row>
+        <v-row>
+            <v-col cols="12" class="text-right">
+                <v-btn class="ml-2" color="primary" @click="onSubmit">작성 완료</v-btn>
+                <v-btn class="ml-1" color="error" @click="onCancel">취소</v-btn>
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
